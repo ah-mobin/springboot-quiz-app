f645630d3473283e4a1b73b0c5c93c8f232d9090
mobin-newroz <ahmobin@newroztech.com>
2022-12-05 17:46:09 +0600

    chore : spring boot app setup for quiz app

---
+++ src/main/java/com/mobin/quizappstarter/QuizAppStarterApplication.java
@@ -0,0 +1,13 @@
+package com.mobin.quizappstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizAppStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mobin/quizappstarter/QuizAppStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobin.quizappstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
